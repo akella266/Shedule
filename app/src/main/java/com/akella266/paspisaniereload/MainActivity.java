61f40ab8dcd3fa6b00f2de14013240d0071a96bf
akella266 <maks.kent266@gmail.com>
2016-12-13 10:15:49 +0300

    Create basics files

---
+++ app/src/main/java/com/akella266/paspisaniereload/MainActivity.java
@@ -0,0 +1,16 @@
+package com.akella266.paspisaniereload;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Akella266 on 13.12.2016.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
