578d2900125c5afc105b11c739a7eb1d976c9537
akella266 <maks.kent266@gmail.com>
2016-12-15 10:47:26 +0300

    Began create a toolbar. There is mistake with R

--- app/src/main/java/com/akella266/paspisaniereload/MainActivity.java
+++ app/src/main/java/com/akella266/paspisaniereload/MainActivity.java
@@ -2,15 +2,30 @@ package com.akella266.paspisaniereload;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-/**
- * Created by Akella266 on 13.12.2016.
- */
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends Activity {
+
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+    }
+
+    private void initToolBar(){
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu);
     }
 }
