1a9cd4591dfab528638ebc79add981b563ccd516
akella266 <maks.kent266@gmail.com>
2017-01-04 16:25:09 +0300

    make buttons without touch and listeners. And more, I must will write adapter to tiles in gridView

--- app/src/main/java/com/akella266/paspisaniereload/MainActivity.java
+++ app/src/main/java/com/akella266/paspisaniereload/MainActivity.java
@@ -4,17 +4,31 @@ import android.support.v7.widget.Toolbar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
 
+    String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     Toolbar toolbar;
+    GridView gvMain;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initToolBar();
+        initGridWithButtons();
+    }
+
+    private void initGridWithButtons() {
+        adapter = new ArrayAdapter<String>(this,R.layout.button_week,R.id.tvText, week);
+        gvMain = (GridView)findViewById(R.id.gridView);
+        gvMain.setAdapter(adapter);
+        gvMain.setColumnWidth(400);
+        gvMain.setVerticalSpacing(10);
+        gvMain.setHorizontalSpacing(10);
     }
 
     private void initToolBar(){
